fd65290f07cd3cf37e0ff74b9a7e9623a6c3a0a7
Timothy Cevallos <timcev@gmail.com>
2017-08-30 17:50:06 +0000

    Added to computercontroller

--- app/controllers/computer_controller.rb
+++ app/controllers/computer_controller.rb
@@ -24,5 +24,50 @@ class ComputerController < ApplicationController
     end
   end
 
+  get '/computer/:id' do
+    if logged_in?
+      @computer = Computer.find_by_id(params[:id])
+      erb :'computer/computer_show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/computer/:id/edit' do
+      @computer = Computer.find_by_id(params[:id])
+      if logged_in? && @computer.user_id == current_user.id
+       erb :'computer/edit'
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/computer/:id' do
+    if params[:name] == ""
+      redirect to "/computer/#{params[:id]}/edit"
+    else
+      @computer = Computer.find_by_id(params[:id])
+      @computer.name = params[:name]
+      @computer.processor = params[:processor]
+      @computer.processor_speed = params[:processor_speed]
+      @computer.graphics_card = params[:graphics_card]
+      @computer.save
+      redirect to "/computer/#{@computer.id}"
+    end
+  end
+
+  delete '/computer/:id/delete' do
+    if logged_in?
+      @computer = Computer.find_by_id(params[:id])
+      if @computer.user_id == current_user.id
+        @computer.delete
+        redirect to '/computers'
+      else
+        redirect to '/computers'
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
 end
