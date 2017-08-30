2b3f8aaa8d79e6a19a43c52fd595d37aad498ed9
Timothy Cevallos <timcev@gmail.com>
2017-08-30 17:22:42 +0000

    working on computer controller

--- app/controllers/computer_controller.rb
+++ app/controllers/computer_controller.rb
@@ -6,4 +6,23 @@ class ComputerController < ApplicationController
     else
       redirect to '/login'
   end
+
+  get '/computer/create_new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/computer' do
+    if params[:name] == ""
+      redirect to "/computer/create_new"
+    else
+      @computer = current_user.computer.create(name: params[:name], processor: params[:processor], processor_speed: params[:processor_speed], graphics_card: params[:graphics_card])
+      redirect to "/computer/#{@computer.id}"
+    end
+  end
+
+
 end
