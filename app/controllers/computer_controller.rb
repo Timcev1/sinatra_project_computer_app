5e106f43705003abc39255dbeead6df57a775af7
Timothy Cevallos <timcev@gmail.com>
2017-08-30 17:14:23 +0000

    added a edit

--- app/controllers/computer_controller.rb
+++ app/controllers/computer_controller.rb
@@ -1,3 +1,9 @@
 class ComputerController < ApplicationController
-
+  get '/computers' do
+    if logged_in?
+      @computers = Computer.all
+      erb :'computer/computers'
+    else
+      redirect to '/login'
+  end
 end
